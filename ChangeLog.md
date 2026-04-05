5834450bf4b3c5131d2cd700b519a795bb92d3a5
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-04-05 14:00:33 +0200

    Closes #135

--- ChangeLog.md
+++ ChangeLog.md
@@ -2,6 +2,12 @@
 
 All notable changes are documented in this file using the [Keep a CHANGELOG](http://keepachangelog.com/) principles.
 
+## [8.1.0] - 2026-MM-DD
+
+### Added
+
+* [#135](https://github.com/sebastianbergmann/diff/issues/135): Add `$contextLines` constructor parameter on `UnifiedDiffOutputBuilder`
+
 ## [8.0.0] - 2026-02-06
 
 ### Removed
@@ -163,6 +169,7 @@ All notable changes are documented in this file using the [Keep a CHANGELOG](htt
 
 * This component is no longer supported on PHP 5.6
 
+[8.1.0]: https://github.com/sebastianbergmann/diff/compare/8.0.0...main
 [8.0.0]: https://github.com/sebastianbergmann/diff/compare/7.0...8.0.0
 [7.0.0]: https://github.com/sebastianbergmann/diff/compare/6.0.2...7.0.0
 [6.0.2]: https://github.com/sebastianbergmann/diff/compare/6.0.1...6.0.2
--- tests/Output/UnifiedDiffOutputBuilderTest.php
+++ tests/Output/UnifiedDiffOutputBuilderTest.php
@@ -115,4 +115,28 @@ final class UnifiedDiffOutputBuilderTest extends TestCase
 
         $this->assertEmpty($output);
     }
+
+    public function testCustomContextLinesCanBeUsed(): void
+    {
+        $from = "line1\nline2\nline3\nline4\nline5\nline6\nline7\nline8\nline9\nline10\n";
+        $to   = "line1\nline2\nline3\nline4\nLINE5\nline6\nline7\nline8\nline9\nline10\n";
+
+        $differ3 = new Differ(new UnifiedDiffOutputBuilder('', false, 3));
+        $differ1 = new Differ(new UnifiedDiffOutputBuilder('', false, 1));
+        $differ5 = new Differ(new UnifiedDiffOutputBuilder('', false, 5));
+
+        $diff3 = $differ3->diff($from, $to);
+        $diff1 = $differ1->diff($from, $to);
+        $diff5 = $differ5->diff($from, $to);
+
+        $this->assertStringContainsString(' line2', $diff3);
+        $this->assertStringNotContainsString(' line1', $diff3);
+
+        $this->assertStringContainsString(' line4', $diff1);
+        $this->assertStringNotContainsString(' line3', $diff1);
+        $this->assertStringNotContainsString(' line7', $diff1);
+
+        $this->assertStringContainsString(' line1', $diff5);
+        $this->assertStringContainsString(' line9', $diff5);
+    }
 }
